a7f23227d7c0227d6a16375fad77fbed613a32db
Ramzi Deek <ramzideek@gmail.com>
2016-07-26 18:07:17 -0500

    Create home and about page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def home
+  end
+
+  def body_transformation
+  end
+
+  def athletic_performance
+  end
+
+  def prices
+  end
+
+  def health_wellness
+  end
+
+  def physique_contest
+  end
+
+  def forms
+  end
+
+  def contact
+  end
+end
